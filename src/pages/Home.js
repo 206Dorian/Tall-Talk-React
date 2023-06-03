0c96994e9fee0c0de6c8e9ae8dba60970ed3f344
Dorian Birch <206dorian@gmail.com>
2023-06-02 21:02:07 -0500

    initiating pages, components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
+import Questions from "../components/Questions"
 import "./Home.css"
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div>
+
+            <div>This is home</div>
+            <div className="QuestionComponent">Questions
+            <Questions />
+            </div>
+        </div>
+    )
 }
 
-export default Home
+export default Home 
