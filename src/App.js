d9ecfff60ba763074f16e6f42c50610a3cb67cf4
Dorian Birch <206dorian@gmail.com>
2023-06-05 11:03:46 -0500

    debuggingh paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Home from "./pages/Home";
 
 
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
 function App() {
 
   return (
 <div className="App">
 <Home />
+
 </div>
 
 
---
+++ src/pages/QuestionsPage.css
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Questions from '../components/Questions';
+
+const QuestionsPage = () => {
+  return (
+    <div>
+      <Header />
+      <Questions />
+      <Footer />
+    </div>
+  );
+}
+
+export default QuestionsPage;
