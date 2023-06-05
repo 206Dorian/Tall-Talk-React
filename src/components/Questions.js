390570e9f38336dc403fba9d8cd5024c465478ea
Dorian Birch <206dorian@gmail.com>
2023-06-05 11:28:28 -0500

    button issues

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Questions = () => {
+  const [currentQuestion, setCurrentQuestion] = useState('');
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch questions from the database or external data source
+    // Update the questions state with the retrieved data
+    const fetchQuestions = async () => {
+      try {
+        // Perform the data fetching and set the questions state
+        const data = await fetchQuestionsFromDatabase();
+        setQuestions(data);
+      } catch (error) {
+        console.error('Error fetching questions:', error);
+      }
+    };
+
+    fetchQuestions();
+  }, []);
+
+  const generateRandomQuestion = () => {
+    if (questions.length > 0) {
+      const randomIndex = Math.floor(Math.random() * questions.length);
+      setCurrentQuestion(questions[randomIndex]);
+    }
+  };
+
   return (
-    <div className="card">
-      <h3>Question</h3>
-      <p>This is the question text.</p>
+    <div>
+      <h3>{currentQuestion}</h3>
+      <button onClick={generateRandomQuestion}>Next Question</button>
     </div>
   );
 }
