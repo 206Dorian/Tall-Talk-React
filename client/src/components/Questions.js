7f1ce97fdcd06b8ac67a7c0bbdb233c885b9203b
Dorian Birch <206dorian@gmail.com>
2023-06-12 15:36:21 -0500

    debugging why questions arent appearing

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -7,12 +7,17 @@ const Questions = () => {
   const { loading, error, data } = useQuery(GET_QUESTIONS);
   const [currentQuestion, setCurrentQuestion] = useState(null);
 
+  console.log(data);  // logging data here
+  
   // Function to select a random question
   const getQuestion = () => {
     if (!loading && data) {
       const questionList = data.questions;
       const randomIndex = Math.floor(Math.random() * questionList.length);
       setCurrentQuestion(questionList[randomIndex].text);
+    }
+    else {
+      console.log('No question available');
     }
   };
 
