291947b78e42d4b6f1e93fc177886bb69e9659d0
Dorian Birch <206dorian@gmail.com>
2023-06-13 12:09:53 -0500

    styling the nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import './NavBar.css'; // Ensure you import the CSS file
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <div className="NavBar">NavBar</div>
+    <div className="NavBar">
+      NavBar
+      <div className="dropdown">
+        <span>Menu</span>
+        <div className="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
