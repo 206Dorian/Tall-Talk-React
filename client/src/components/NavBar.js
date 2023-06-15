0671e3437f74a7f0d0a0cdd17f4372f24ec2e40c
Dorian Birch <206dorian@gmail.com>
2023-06-14 21:37:19 -0500

    changing Nav Bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,9 +7,9 @@ const NavBar = () => {
       <div className="dropdown">
         {/* <button className="dropbtn">Dropdown</button> */}
         <div className="dropdown-content">
-          <a href="#"> Add a Question</a>
-          <a href="#"> Befriend</a>
-          <a href="#"> Comment or like</a>
+          <a href="#"> A</a>
+          <a href="#"> B</a>
+          <a href="#"> C</a>
         </div>
       </div>
     </nav>
