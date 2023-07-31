c712d811d1b34299b7999cb537591ee34a792567
Dorian Birch <206dorian@gmail.com>
2023-07-31 11:33:58 -0500

    getting diff errors, almost there with heroku

--- server/server.js
+++ server/server.js
@@ -50,7 +50,6 @@ const startApolloServer = async () => {
   
 
 
-
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
