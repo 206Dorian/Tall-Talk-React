abfe00c6a3d5c8e72355cb95238eb2b864a3b532
Dorian Birch <206dorian@gmail.com>
2023-09-23 16:20:35 -0500

    updated favicon and change dmy oprtfolio link in the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
         <br/>
         <a href="https://www.linkedin.com/in/dorian-douglas-birch-70695b4b/">LinkedIn</a>
         <br/>
-        <a href="https://206dorian.github.io/dorian-portfolio/">Portfolio</a>
+        <a href="https://dorians-portfolio-d5232a4cbe01.herokuapp.com/">Portfolio</a>
         <br></br>
         <br/>
       </footer>
