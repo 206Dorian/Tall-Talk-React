32529167eab4a5902b3295d45ceff8acb40d2879
Dorian Birch <206dorian@gmail.com>
2023-09-23 11:39:21 -0500

    updated so i shuffle through each question without duplicating

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import QButton from './QButton';
-import { GET_QUESTIONS } from '../utils/queries'
+import { GET_QUESTIONS } from '../utils/queries';
 import './Questions.css';
 
 const Questions = () => {
   const { loading, error, data } = useQuery(GET_QUESTIONS);
+  const [questions, setQuestions] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(null);
 
-  console.log(data);  // logging data here
-  
-  // Function to select a random question
-  const getQuestion = () => {
-    if (!loading && data) {
-      const questionList = data.questions;
-      const randomIndex = Math.floor(Math.random() * questionList.length);
-      setCurrentQuestion(questionList[randomIndex].text);
+  useEffect(() => {
+    // Check if data is available and questions have not been set yet
+    if (!loading && data && questions.length === 0) {
+      // Shuffle the questions randomly
+      const shuffledQuestions = shuffleArray(data.questions);
+      setQuestions(shuffledQuestions);
     }
-    
-    else {
-      console.log('No question available');
+  }, [loading, data, questions]);
+
+  const shuffleArray = (array) => {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  };
+
+  const getNextQuestion = () => {
+    if (questions.length > 0) {
+      // Get and remove the next question from the shuffled list
+      const nextQuestion = questions.pop();
+      setCurrentQuestion(nextQuestion.text);
+    } else {
+      // All questions have been displayed, reset the list
+      setQuestions(shuffleArray(data.questions));
+      getNextQuestion();
     }
   };
 
@@ -28,13 +44,13 @@ const Questions = () => {
 
   return (
     <div>
-    <div className='card'>
-            <br/>
-      <p>{currentQuestion}</p>
-      <br/>
+      <div className='card'>
+        <br />
+        <p>{currentQuestion}</p>
+        <br />
+      </div>
+      <QButton onClick={getNextQuestion} />
     </div>
-    <QButton onClick={getQuestion} />
-  </div>
   );
 };
 
