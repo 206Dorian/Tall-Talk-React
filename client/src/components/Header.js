7eb73f355afc8ebd8a5c0a0f9c31358880c39499
Dorian Birch <206dorian@gmail.com>
2024-11-12 16:58:02 -0600

    updating name of app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,8 +6,11 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <h1>Tall Talk</h1>
-      <h2>Because I have no time for small talk...</h2>
+      {/* <h1>Tall Talk</h1> */}
+      <br></br>
+      <h1>QUERIES & QUANDRIES</h1>
+      <br></br>
+      <h2>Think of this as the opposite of small talk...</h2>
     </header>
   );
 }
